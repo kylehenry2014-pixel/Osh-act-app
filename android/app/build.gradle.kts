46fc8ea46de4587067c0e3076ae293031135f479
kylehenry2014-pixel <kylehenry2014@gmail.com>
2026-09-20 19:51:08 +0200

    Downgrade Flutter to 3.29.3 to avoid appbundle stripping bug

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
             isMinifyEnabled = false
             isShrinkResources = false
             ndk {
-                debugSymbolLevel = "FULL"
+                debugSymbolLevel = "NONE"
             }
         }
     }
